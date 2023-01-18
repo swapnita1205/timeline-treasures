1ddb67d3e9daea9fe9615f177e1d15055b9c7022
swapnita <swapnita1205@gmail.com>
2023-01-19 04:11:41 +0530

    test7

--- iblogs/settings.py
+++ iblogs/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9f)bt!1hl3=b!wri+o_gtzf5pm_ffmq$2$oa(k&vb6ilb56ueb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
+ALLOWED_HOSTS = ['web-production-3aa7.up.railway.app', '127.0.0.1']
 
 
 # Application definition
