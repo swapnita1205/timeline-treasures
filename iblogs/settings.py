93aa89e3687d3cf195c5990dcb21e0d90a642701
swapnita <swapnita1205@gmail.com>
2023-01-19 03:10:35 +0530

    done vercel setting

--- iblogs/settings.py
+++ iblogs/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9f)bt!1hl3=b!wri+o_gtzf5pm_ffmq$2$oa(k&vb6ilb56ueb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['timeline-treasures.herokuapp.com']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
 
 # Application definition
@@ -120,8 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+# STATIC_URL = 'static/'
+
+import os
+STATIC_URL= 'static/',
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 
 MEDIA_URL = '/media/'
 
