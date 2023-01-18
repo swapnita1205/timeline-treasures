ba8ad86ebc0711aefb79d9cff670854bffc59e3a
swapnita <swapnita1205@gmail.com>
2023-01-19 03:26:24 +0530

    test1

--- iblogs/settings.py
+++ iblogs/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 import os
 STATIC_URL= 'static/',
 STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_URL = '/media/'
 
